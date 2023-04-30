2f59f2a929bfce099f104c89f29bce23541cc25a
CamperoJose <jose.campero@ucb.edu.bo>
2023-04-30 11:36:42 -0400

    creando cubit y states para creacion de labels

---
+++ lib/bl/label_create_state.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+enum LabelCreateStatus { init, loading, success, failure }
+
+class LabelCreateState extends Equatable {
+  final LabelCreateStatus status;
+
+  const LabelCreateState({
+    this.status = LabelCreateStatus.init,
+  });
+
+  LabelCreateState copyWith({
+    LabelCreateStatus? status,
+  }) {
+    return LabelCreateState(
+      status: status ?? this.status,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status];
+}
